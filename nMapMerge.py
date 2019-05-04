0edadd0892aa584a0fc24f1d703b682773fc813d
cbhue <cbhue@users.noreply.github.com>
2019-05-04 19:34:15 -0400

    Migrated to python3

--- nMapMerge.py
+++ nMapMerge.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 ####################################################################################
 # 
 # nMapMerge.py 
@@ -14,121 +15,143 @@
 #
 #
 ####################################################################################
-#!/usr/bin/env python
-
-import xml.etree.ElementTree as ET
-from xml.etree.ElementTree import ParseError
-
-import logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-print 'Debug On'
 
 import os
 import re
 import time
-
+import logging
+import xml.etree.ElementTree as ET
 from argparse import ArgumentParser
-parser = ArgumentParser()
-parser.add_argument("-f", "--file", dest="filename",
-                    help="parse FILE", metavar="FILE")
-parser.add_argument("-d", "--dir", dest="directory",
-                    help="Parse all xml in directory", metavar="DIR")
-parser.add_argument("-q", "--quiet",
-                    action="store_false", dest="verbose", default=True,
-                    help="don't print status messages to stdout")
+from xml.etree.ElementTree import ParseError
 
-def merge_nMap(f, mf):
+def merge_nMap(xmlFile,mf):
 	HOSTS = 0
-	try: 
-		nMapXML = ET.ElementTree(file=f);
-		
-		for host in nMapXML.findall('host'):
-			HOSTS = HOSTS + 1
-			cHost = ET.tostring(host)  
-			mFile = open(mf, "a")  
-			mFile.write(cHost) 
-			mFile.close()	
-
-	except Exception, e:
-		logging.warn("skipping: %r", f)
-		logging.warn(str(e))
-
+	with open(mf, mode = 'a', encoding='utf-8') as mergFile:
+		with open(xmlFile) as f:
+			nMapXML = ET.parse(f)
+			for host in nMapXML.findall('host'):
+				HOSTS = HOSTS + 1
+				cHost = ET.tostring(host, encoding='unicode', method='xml') 
+				mergFile.write(cHost)
+				mergFile.flush()	
 	return HOSTS
 
 def addHeader(f):
-	nMap_Header  = '<?xml version="1.0" encoding="UTF-8"?>\n'
-	nMap_Header += '<!DOCTYPE nmaprun>\n'
-	nMap_Header += '<?xml-stylesheet href="file:///usr/share/nmap/nmap.xsl" type="text/xsl"?>\n'
-	nMap_Header += '<!-- Nmap Merged with nMapMergER.py https://github.com/CBHue/nMapMergER -->\n'
-	nMap_Header += '<nmaprun scanner="nmap" args="nmap -iL hostList.txt" start="1" startstr="Wed Sep  0 00:00:00 0000" version="7.70" xmloutputversion="1.04">\n'
-	nMap_Header += '<scaninfo type="syn" protocol="tcp" numservices="1" services="1"/>\n'
-	nMap_Header += '<verbose level="0"/>\n'
-	nMap_Header += '<debugging level="0"/>\n'
+	nMap_Header  = '<?xml version="1.0" encoding="UTF-8"?>'
+	nMap_Header += '<!DOCTYPE nmaprun>'
+	nMap_Header += '<?xml-stylesheet href="file:///usr/share/nmap/nmap.xsl" type="text/xsl"?>'
+	nMap_Header += '<!-- Nmap Merged with nMapMergER.py https://github.com/CBHue/nMapMergER -->'
+	nMap_Header += '<nmaprun scanner="nmap" args="nmap -iL hostList.txt" start="1" startstr="https://github.com/CBHue/nMapMerge/nMapMerge.py" version="7.70" xmloutputversion="1.04">'
+	nMap_Header += '<scaninfo type="syn" protocol="tcp" numservices="1" services="1"/>'
+	nMap_Header += '<verbose level="0"/>'
+	nMap_Header += '<debugging level="0"/>'
 
 	mFile = open(f, "w")  
 	mFile.write(nMap_Header) 
 	mFile.close()
 
 def addFooter(f, h):
-	nMap_Footer  = '<runstats><finished time="1" timestr="Wed Sep  0 00:00:00 0000" elapsed="0" summary="Nmap done at Wed Sep  0 00:00:00 0000; ' + str(h) + ' IP address scanned in 0.0 seconds" exit="success"/>\n'
-	nMap_Footer += '</runstats>\n'
-	nMap_Footer += '</nmaprun>\n'
+	nMap_Footer  = '<runstats><finished time="1" timestr="Wed Sep  0 00:00:00 0000" elapsed="0" summary="Nmap done at Wed Sep  0 00:00:00 0000; ' + str(h) + ' IP address scanned in 0.0 seconds" exit="success"/>'
+	nMap_Footer += '</runstats>'
+	nMap_Footer += '</nmaprun>'
 
 	mFile = open(f, "a")  
 	mFile.write(nMap_Footer) 
 	mFile.close()
 
-def main(mf):
-	args = parser.parse_args()
-	HOSTS = 0
-
-	if args.filename is not None:
-		f = args.filename
-		if f.endswith('.xml'):
-			logging.debug("FILE: %r", f)
-			H = merge_nMap(f,mf)
-			HOSTS = HOSTS + H
-
-	elif args.directory is not None:
-		path = args.directory
-
-		for f in os.listdir(path):
-			# For now we assume xml is nMap
-			if f.endswith('.xml'): 
-				fullname = os.path.join(path, f)
-				logging.debug("PATH: %r", fullname)
-				H = merge_nMap(fullname,mf)
-				HOSTS = HOSTS + H
-
-	else :
-		print "usage issues =("
-		exit
-
-	return HOSTS
-
-if __name__ == "__main__":
-	
-	from datetime import datetime
-	dt = datetime.now() 
-	dt = re.sub(r"\s+", '-', str(dt))
-	dt = re.sub(r":", '-', str(dt))
-	mergeFile = "nMap_Merged_" + dt + ".xml"
-
-	# Create Header
-	addHeader(mergeFile)
-
-    # call main 
-	H = main(mergeFile)
-
-	# create Footer
-	addFooter(mergeFile, H)
-
-	print "Output XML File:", mergeFile
-
+def htmlER(mergeFile):
 	import os
 	cmd = '/usr/bin/xsltproc'
 	if os.path.isfile(cmd):
 		out = re.sub(r'.xml', '.html', mergeFile)
 		cmd = cmd + " -o " + out + " " + mergeFile
 		os.system(cmd)
-		print "Output HTML File:", out
+		print ("Output HTML File:", out)
+	else:
+		print(cmd, "does not exits")
+
+#
+# If you want to use this as a module you need to pass a set of nmap xmls
+#
+# nmapSET = set()
+# nmapSET.add('/nmap-Dir/nmap_10.10.10.10.xml')
+#
+# Then call the main function passing the set:
+# main_nMapMerger(nmapSET)
+#
+def main_nMapMerger(xmlSet):
+	HOSTS = 0
+
+	# Check to ensute we have work to do
+	if not xmlSet:
+		print("No XML files were found ... No work to do")
+		exit()
+
+	# Create the Merged filename
+	from datetime import datetime
+	dtNow = datetime.now() 
+	dt = re.sub(r"\s+", '-', str(dtNow))
+	dt = re.sub(r":", '-', str(dt))
+	mergeFile = "nMap_Merged_" + dt + ".xml"
+
+	# Add Header to mergefile
+	addHeader(mergeFile)
+
+	for xml in xmlSet:
+		if xml.endswith('.xml'):
+			logging.debug("Parsing: %r", xml)
+			H = merge_nMap(xml,mergeFile)
+			HOSTS = HOSTS + H
+
+	# Add Footer to mergefile
+	addFooter(mergeFile, HOSTS)
+	print('')
+	print ("Output XML File:", mergeFile)
+
+	# Convert merged XML to html
+	htmlER(mergeFile)
+
+if __name__ == "__main__":
+	
+	import sys
+	if sys.version_info <= (3, 0):
+		sys.stdout.write("This script requires Python 3.x\n")
+		sys.exit(1)
+
+	parser = ArgumentParser()
+	parser.add_argument("-f", "--file", 	dest="filename", help="parse FILE", metavar="FILE")
+	parser.add_argument("-d", "--dir", 		dest="directory", help="Parse all xml in directory", metavar="DIR")
+	parser.add_argument("-q", "--quiet",	dest="verbose",	action="store_false", default=True, help="don't print status messages to stdout")
+	args = parser.parse_args()
+
+	s = set()
+	
+	if args.verbose:
+		logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+		print('Debug On')
+
+	if args.filename is not None:
+		f = args.filename
+		if f.endswith('.xml'):
+			logging.debug("Adding: %r", f)
+			s.add(f)
+
+	elif args.directory is not None:
+		if os.path.isdir(args.directory):
+			path = args.directory
+			
+			for f in os.listdir(path):
+				# For now we assume xml is nMap
+				if f.endswith('.xml'): 
+					fullname = os.path.join(path, f)
+					logging.debug("Adding: %r", fullname)
+					s.add(fullname)
+		else:
+			logging.warn("Not a directory: %r", args.directory)
+	else :
+		print ("usage issues =(")
+		parser.print_help()
+		exit()
+
+	# Pass set of xml files to main
+	main_nMapMerger(s)
